a36acc073eb4396aa83e774024928091e1c8e4f7
Maghfirah Suyuti <60200112023@uin-alauddin.ac.id>
2015-05-03 18:39:19 +0800

    Game
    Maghfirah Suyuti
    60200112023

---
+++ Assets/script/move.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public float speed;
+	public float lompat;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+	// Use this for initialization
+	void Start () {
+
+	}
+	void FixedUpdate(){
+		grounded = Physics.CheckSphere (groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && grounded){
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x, lompat);
+			audio.Play();
+		}
+		if (Input.GetKey(KeyCode.RightArrow)){
+			rigidbody.velocity = new Vector3(speed,rigidbody.velocity.y);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			rigidbody.velocity = new Vector3(-speed,rigidbody.velocity.y);
+		}
+	}
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Game_023
-Pembuatan Game, Maghfirah Suyuti 60200112023
-Karakter belum fix, story masi di buat
