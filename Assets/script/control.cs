ef9f23b0d78c9e6eb1024b7ed9a8bf79ceed79f3
Maghfirah Suyuti <60200112023@uin-alauddin.ac.id>
2015-05-03 12:13:58 +0800

    Pembuatan Game, Maghfirah Suyuti 60200112023
    Karakter masih belum fix, story line masi d pikir

---
+++ Assets/script/control.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control : MonoBehaviour {
+	public float moveSpeed;
+	public float jumpHeight;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	void FixedUpdate(){
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && grounded)
+	    {
+			rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, jumpHeight);
+			audio.Play();
+
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			rigidbody2D.velocity = new Vector2 (-moveSpeed,rigidbody2D.velocity.y);
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			rigidbody2D.velocity = new Vector2 (moveSpeed,rigidbody2D.velocity.y);
+		}
+		anim.SetFloat ("right", rigidbody2D.velocity.x);
+		anim.SetFloat("left",-rigidbody2D.velocity.x);
+	
+	}
+}
